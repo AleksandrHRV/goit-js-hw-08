69b56bf24272968034e1f0316c3b24045055adf0
Aleksander <guree.19.05.94@gmail.com>
2023-03-11 13:45:46 +0200

    1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,19 +2,23 @@ var throttle = require('lodash.throttle');
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
-console.log(localStorage.getItem("videoplayer-current-time", "second"));
 //=========================================================================================================================================
-player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then(function (seconds) {
-}).catch(function (error) {
+player
+  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .then(function (seconds) {})
+  .catch(function (error) {
     switch (error.name) {
-        case 'RangeError':
-            break;
+      case 'RangeError':
+        break;
 
-        default:
-            break;
+      default:
+        break;
     }
-});
-player.on('timeupdate', throttle((time) => {
-    localStorage.setItem("videoplayer-current-time", `${time.seconds}`);
-}, 1000)
+  });
+
+player.on(
+  'timeupdate',
+  throttle(time => {
+    localStorage.setItem('videoplayer-current-time', `${time.seconds}`);
+  }, 1000)
 );
